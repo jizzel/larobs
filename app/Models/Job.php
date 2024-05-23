c36b7d624dd556622d96a50ae289cdbf8a3af047
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-23 16:35:38 +0000

    feat: update job listings

---
+++ app/Models/Job.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model {
+    use HasFactory;
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+    }
+}
