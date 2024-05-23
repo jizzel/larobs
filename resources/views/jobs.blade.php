ea902c9a7a19f4d2bf78a9f35ea895c804354cec
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-23 17:26:34 +0000

    feat: add styling, pagination and debugger

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,13 +4,19 @@
     </x-slot:heading>
     Jobs
     <br>
-    <ul>
+    <div class="space-y-4">
     @foreach($jobs as $job)
-            <li>
-                <a href="/job/{{ $job['id'] }}" class="text-blue hover:underline">
+            <a href="/job/{{ $job['id'] }}" class="block ps-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">
+                    {{ $job->employer->name }}
+                </div>
+                <div>
                     <strong>{{ $job['title'] }}: </strong>Pays {{ $job['salary'] }} per year.
-                </a>
-            </li>
+                </div>
+            </a>
     @endforeach
-    </ul>
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
 </x-layout>
