cc1d3ad77f3c0de1878f5afe132a9a06fb715026
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-24 14:51:40 +0000

    ref: group controllers

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,14 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
-
-//index
-Route::get('jobs', [JobController::class, 'index'])->name('jobs.index');
-
-//create
-Route::get('job/create', [JobController::class, 'create'])->name('jobs.create');
-
-//show
-Route::get('job/{job}', [JobController::class, 'show'])->name('jobs.show');
-
-//store
-Route::post('job', [JobController::class, 'store'])->name('jobs.store');
-
-//edit
-Route::get('job/{job}/edit', [JobController::class, 'edit'])->name('jobs.edit');
-
-//update
-Route::patch('job/{job}', [JobController::class, 'update'])->name('jobs.update');
-
-//destroy
-Route::delete('job/{job}', [JobController::class, 'destroy'])->name('jobs.destroy');
-
 Route::view('contact', 'contact')->name('contact');
+
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index')->name('jobs');
+    Route::get('/job/create', 'create')->name('jobs.create');
+    Route::post('/job', 'store')->name('jobs.store');
+    Route::get('/job/{job}', 'show')->name('jobs.show');
+    Route::get('/job/{job}/edit', 'edit')->name('jobs.edit');
+    Route::patch('/job/{job}', 'update')->name('jobs.update');
+    Route::delete('/job/{job}', 'destroy')->name('jobs.destroy');
+});
