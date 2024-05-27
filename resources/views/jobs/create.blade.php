dd713f4bdb18014955d05d38e7bb66f14f269289
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-27 11:11:39 +0000

    ref: refactor into components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -24,29 +24,21 @@
                 <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Software Engineer" required>
-                            </div>
-                            @error('title')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input type="text" name="title" id="title" autocomplete="title" placeholder="Software Engineer" required></x-form-input>
+                            <x-form-error name="title"></x-form-error>
                         </div>
-                    </div>
+                    </x-form-field>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="title">Salary</x-form-label>
                         <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary" autocomplete="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000" required>
-                            </div>
-                            @error('salary')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-input type="text" name="salary" id="salary" autocomplete="salary" placeholder="$50,000" required></x-form-input>
+                            <x-form-error name="salary"></x-form-error>
                         </div>
-                    </div>
+                    </x-form-field>
 
                 </div>
 
@@ -65,7 +57,7 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button>Save</x-form-button>
         </div>
     </form>
 
