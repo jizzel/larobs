031abb061a9a5defa452b30263189ded8ad0ad48
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-27 15:33:19 +0000

    feat: add email sender

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -24,11 +26,13 @@ class JobController extends Controller
             'title' => ['required', 'min:3'],
             'salary' => 'required',
         ]);
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user)->send(new JobPosted($job));
 
         return redirect('/jobs');
     }
---
+++ resources/views/mail/job-posted.blade.php
@@ -0,0 +1,6 @@
+<h2>{{ $job->title }}</h2>
+<p>Congrats! Your job is now live on our website!</p>
+
+<p>
+    <a href="{{ url('/jobs/'.$job->id) }}">View Job Listing</a>
+</p>
